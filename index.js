d69350fe2a00492e25fafcf3778331073743f21b
Reza Lotfi <rezaiscool7@aol.com>
2018-08-10 06:30:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
+  "He hated the sound of his own voice"]
   while (i < 100){
+    var i = 1
     
   }
-]
 }
