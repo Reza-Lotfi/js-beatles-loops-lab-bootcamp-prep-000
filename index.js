76763102860e8d71cd7ef0add151f708123b4f5a
Reza Lotfi <rezaiscool7@aol.com>
2018-08-10 05:19:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+function theBeatlesPlay(musicians, instruments){
+var array = []
+for (var i = 0; i < 4; i++){
+  array.push(`musicians[${i}] plays instruments[${i}]`)
+}
+  return array
+}
