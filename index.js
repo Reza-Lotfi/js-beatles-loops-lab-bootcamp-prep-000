042c54e20de9c362a41b971c11ce3e88ef2f722f
Reza Lotfi <rezaiscool7@aol.com>
2018-08-10 07:04:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,9 @@ function johnLennonFacts(facts){
 
 function iLoveTheBeatles(Number){
   var array = []
-  do{
-    array['I love the Beatles!']
-    number++
-  }while
+    do{
+      array['I love the Beatles!']
+      number++
+    }while (number < 15)
+  return array
 }
