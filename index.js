ac148ab390b1a63042e6ed5e6b4b661b27f60468
Reza Lotfi <rezaiscool7@aol.com>
2018-08-10 06:27:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,12 @@ for (var i = 0; i < 4; i++){
   return array
 }
 
-function johnLennonFacts(facts)
+function johnLennonFacts(facts){
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  while 
+]
+}
